8cdcdddff95323933cb57ea7816c66d25af22868
Kevin Kragenbrink <kevin@writh.net>
2012-10-12 12:15:07 -0700

    Removing window overrides to prevent collisions with crc package.

--- src/Class.js
+++ src/Class.js
@@ -7,7 +7,7 @@
  *     \/  \/ |_|  |_|\__|_| |_(_)_| |_|\___|\__|
  *
  * @created     2012-02-08
- * @edited      2012-10-05
+ * @edited      2012-10-12
  * @package     Libraries
  * @see         https://github.com/Writh/classical
  *
@@ -34,7 +34,6 @@
 (function() {
 if (typeof global == 'undefined') { global = window; }
 if (typeof global == 'undefined') { global = {}; }
-if (typeof window == 'undefined') { window = global; }
 if (typeof module == 'undefined') { module = {}; }
 
 /**
